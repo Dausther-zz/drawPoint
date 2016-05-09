0b82878d7e7890aa4d16b19afa9488ab4d0374bb
Dausther Seidel Perini <dausther@gmail.com>
2016-05-09 14:56:48 -0300

    Initial commit

---
+++ src/point/DrawPoint.java
@@ -0,0 +1,16 @@
+package point;
+
+public class DrawPoint {
+
+	public static void main(String[] args) {
+		Point lowerLeft = new Point();
+		Point upperRight = new Point();
+		Point middlePoint = new Point();
+		lowerLeft.x = 0.0;
+		lowerLeft.y = 0.0;
+		upperRight.x = 1280.0;
+		upperRight.y = 1024.0;
+		middlePoint.x = 640.0;
+		middlePoint.y = 612.0;
+	}
+}
